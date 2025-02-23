93fcbbef461e6024a942517c7e3d2177f016f316
zyad113 <zyadhassana6@gmail.com>
2025-02-23 15:03:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('ZR1 Tech Project Loaded');
-});
+document.getElementById('year').textContent = new Date().getFullYear();
+
+const toggleMenu = () => {
+    const navLinks = document.getElementById('navLinks');
+    navLinks.classList.toggle('active');
+};
